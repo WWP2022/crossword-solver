9cebde11567b817a1786917e2d4ca4aded139258
speedy <k.w1@o2.pl>
2022-10-31 11:08:30 +0100

    Initialize camera in proper view

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,5 @@
-import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
 import 'view/app.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  List<CameraDescription> cameras = await availableCameras();
-  final CameraDescription camera = cameras.first;
-  runApp(App(camera));
-}
+void main() => runApp(const App());
