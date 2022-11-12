91252181ce4f0f6b2678bb62b6ae1af263484670
Przemysław Wątroba <przemyslaw.watroba@gmail.com>
2022-11-12 14:32:36 +0100

    Add deleting images from MyCrossword view.

--- lib/view/my_crosswords.dart
+++ lib/view/my_crosswords.dart
@@ -7,13 +7,38 @@ import 'package:flutter/services.dart';
 import '../database/photoRepository.dart';
 import '../model/photo.dart';
 
-class MyCrosswords extends StatelessWidget {
+class MyCrosswords extends StatefulWidget {
   const MyCrosswords({super.key});
+
+  @override
+  State<MyCrosswords> createState() => MyCrosswordsState();
+}
+
+class MyCrosswordsState extends State<MyCrosswords> {
+
+  late List<Photo> photos;
+
+  @override
+  void initState() {
+    super.initState();
+    getPhotos();
+  }
+
+  getPhotos() async {
+    PhotoRepository photoRepository = PhotoRepository();
+    photos = await photoRepository.getAllPhotos();
+    setState(() {});
+  }
 
   Future<List<Photo>> getImages() async {
     PhotoRepository photoRepository = PhotoRepository();
     List<Photo> photos = await photoRepository.getAllPhotos();
     return photos;
+  }
+
+  void removeImage(int id) async {
+    PhotoRepository photoRepository = PhotoRepository();
+    await photoRepository.deletePhoto(id);
   }
 
   @override
@@ -45,27 +70,41 @@ class MyCrosswords extends StatelessWidget {
 
     return Container(
         margin: const EdgeInsets.only(bottom: 5.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            const Expanded(flex: 25, child: SizedBox()),
-            Expanded(
-                flex: 300,
-                child: GestureDetector(
-                  child: image,
-                  onTap: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (_) {
-                      return DetailScreen(image);
-                    }));
-                  },
-                )),
-            const Expanded(flex: 25, child: SizedBox()),
-            Expanded(flex: 300, child: Text(crosswordName)),
-            const Expanded(flex: 25, child: SizedBox()),
-            Expanded(flex: 300, child: Text(date)),
-            const Expanded(flex: 25, child: SizedBox()),
-          ],
-        ));
+        child: Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+          const Expanded(flex: 25, child: SizedBox()),
+          Expanded(
+              flex: 300,
+              child: GestureDetector(
+                child: image,
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (_) {
+                    return DetailScreen(image);
+                  }));
+                },
+              )),
+          const Expanded(flex: 25, child: SizedBox()),
+          Expanded(flex: 300, child: Text(crosswordName)),
+          const Expanded(flex: 25, child: SizedBox()),
+          Expanded(flex: 300, child: Text(date)),
+          const Expanded(flex: 25, child: SizedBox()),
+          Expanded(
+            flex: 300,
+            child: IconButton(
+              onPressed: () {
+                setState(() {
+                  photos.remove(photo);
+                });
+                // removing crossword also from database
+                removeImage(photo.id!);
+              },
+              icon: const Icon(
+                Icons.delete,
+                size: 40.0,
+                color: Colors.red,
+              ),
+            ),
+          ),
+        ]));
   }
 }
 
